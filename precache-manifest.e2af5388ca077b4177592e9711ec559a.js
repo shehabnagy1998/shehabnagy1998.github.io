99d5f8e3fe5cc818d5185f082b72a5a2ca1cebbc
shehabnagy1998 <shehab.nagy1998@gmail.com>
2019-06-14 10:23:43 +0200

    finished

---
+++ precache-manifest.e2af5388ca077b4177592e9711ec559a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5620604f6b3695b8d75b6410e484a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4416f577fcddae10f9e6",
+    "url": "/static/css/main.46c748c2.chunk.css"
+  },
+  {
+    "revision": "967fc2171ad7ee7f509f",
+    "url": "/static/js/2.725c8d80.chunk.js"
+  },
+  {
+    "revision": "4416f577fcddae10f9e6",
+    "url": "/static/js/main.db3bd7a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
